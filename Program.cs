d75fb6ec4e14ac81e54e66ac0d0e134eae5d08ef
eliodmsr <eliodmsr@gmail.com>
2023-11-04 15:39:37 -0300

    final version of challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia1 = new Nokia("+55 11 91234-5544", "G21", "443282538929695", 64);
+
+nokia1.InstalarAplicativo("WhatsApp");
+
+Iphone iphone1 = new Iphone("+55 11 97245-5984", "iPhone 14 Pro Max", "991445358940235", 256);
+
+iphone1.InstalarAplicativo("Instagram");
